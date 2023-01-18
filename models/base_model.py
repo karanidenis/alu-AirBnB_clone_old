e29aa0668fe24fbe786224a06e5abc4c40af16c3
karanidenis <d.waweru@alustudent.com>
2023-01-18 18:56:06 +0300

    Base model edit

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,12 @@
 # The basemodel of the AIRBnB clone
 
 from uuid import uuid4
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """ base class for all classes"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -15,3 +15,14 @@ class BaseModel:
     def __str__(self):
         """readable representation"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.created_at = datetime.now()
+    
+    def to_dict(self):
+        my_dictionary = self.__dict__.copy()
+        my_dictionary['__class__']= self.__class__.__name__
+        my_dictionary['created_at'] = self.created_at.isoformat()
+        my_dictionary['updated_at'] =  self.updated_at.isoformat()
+        return my_dictionary
+
