c8a63562b211e4657576540c6c4cb56f6280c552
karanidenis <d.waweru@alustudent.com>
2023-01-18 18:05:46 +0300

    airbnb

---
+++ models/base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# The basemodel of the AIRBnB clone
+
+from uuid import uuid4
+import datetime
+
+class BaseModel:
+    """ base class for all classes"""
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """readable representation"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
